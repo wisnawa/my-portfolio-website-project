5e682ab9d2477ab8c7dfd69bf0e63918306d6e55
wisnawa <putu.wisnawa@gmail.com>
2023-02-02 15:00:24 +0800

    modified style, main.js, index files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,22 @@
 /*=============== FILTERS TABS ===============*/
-
+const tabs = document.querySelectorAll("[data-target]"),
+  tabContents = document.querySelectorAll("[data-content]");
+tabs.forEach((tab) => {
+  tab.addEventListener("click", (e) => {
+    const target = document.querySelector(tab.dataset.target);
+    tabContents.forEach((tc) => {
+      /*tc = tabcontent */
+      tc.classList.remove("filters__active");
+    });
+    target.classList.add("filters__active");
+    tabs.forEach((t) => {
+      /* t = tab */
+      t.classList.remove("filter-tab-active");
+    });
+    tab.classList.add("filter-tab-active");
+  });
+});
 
 /*=============== DARK LIGHT THEME ===============*/
-
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
